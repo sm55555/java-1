590a245793bb3ced2219b77fe8929f6c64f4aee2
jokerKwu <pkjhj485@naver.com>
2018-12-18 10:55:17 +0900

    BOJ_9536

---
+++ JavaStructure/src/BOJ/BOJ_9536.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+public class BOJ_9536 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		HashMap<String,String> animal=new HashMap();
+		int testCase=input.nextInt();
+		input.nextLine();
+		for(int i=0;i<testCase;i++) {
+			boolean run=true;
+			String str=input.nextLine();
+			String[] woolTokens=str.split(" ");//울음소리 토큰
+			while(run) {
+				String word=input.nextLine();
+				String[] wordTokens=word.split(" ");
+				if(!wordTokens[1].equals("goes")) {
+					run=false;
+				}else {
+					animal.put(wordTokens[0], wordTokens[2]);
+				}
+			}
+			for(int j=0;j<woolTokens.length;j++) {
+				if(!animal.containsValue(woolTokens[j])) {
+					System.out.print(woolTokens[j]+" ");
+				}
+			}System.out.println();
+		}
+	}
+}
+
