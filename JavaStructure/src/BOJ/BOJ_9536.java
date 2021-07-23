b32e0a06e22cabe5438ce4a30fb4f4af9dd35ab5
sm55555 <38831314+sm55555@users.noreply.github.com>
2021-07-23 10:57:53 +0900

    Update BOJ_9536.java

--- JavaStructure/src/BOJ/BOJ_9536.java
+++ JavaStructure/src/BOJ/BOJ_9536.java
@@ -8,13 +8,13 @@ import java.util.Set;
 public class BOJ_9536 {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
-		HashMap<String,String> animal=new HashMap();
 		int testCase=input.nextInt();
 		input.nextLine();
 		for(int i=0;i<testCase;i++) {
 			boolean run=true;
+			HashMap<String,String> animal=new HashMap();
 			String str=input.nextLine();
-			String[] woolTokens=str.split(" ");//울음소리 토큰
+			String[] woolTokens=str.split(" ");
 			while(run) {
 				String word=input.nextLine();
 				String[] wordTokens=word.split(" ");
